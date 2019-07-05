5f8bf7f99541c0cd73b59b9d449028777022fdc7
xLasercut <tianxun.wang2004@gmail.com>
2019-07-05 12:25:08 +0100

    deploy

---
+++ examples/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <nhs-table
+    :headers="headers"
+    :data="data"
+    caption="Skin symptoms and possible causes"
+  >
+    <template #item="item">
+      <nhs-table-item>{{item.props.symptoms}}</nhs-table-item>
+      <nhs-table-item>{{item.props.cause}}</nhs-table-item>
+    </template>
+  </nhs-table>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        data: [
+          {
+            symptoms: 'Blisters on lips or around the mouth',
+            cause: 'cold sores'
+          },
+          {
+            symptoms: 'Itchy, dry, cracked, sore',
+            cause: 'eczema'
+          },
+          {
+            symptoms: 'Itchy blisters',
+            cause: 'shingles, chickenpox'
+          }
+        ],
+        headers: [ 'Skin symptoms', 'Possible cause' ]
+      }
+    }
+  }
+</script>
---
+++ examples/TablePanel.vue
@@ -0,0 +1,37 @@
+<template>
+  <nhs-table
+    :headers="headers"
+    :data="data"
+    heading="Conditions similar to impetigo"
+    caption="Other possible causes of your symptoms"
+  >
+    <template #item="item">
+      <nhs-table-item>{{item.props.symptoms}}</nhs-table-item>
+      <nhs-table-item>{{item.props.cause}}</nhs-table-item>
+    </template>
+  </nhs-table>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        data: [
+          {
+            symptoms: 'Blisters on lips or around the mouth',
+            cause: 'cold sores'
+          },
+          {
+            symptoms: 'Itchy, dry, cracked, sore',
+            cause: 'eczema'
+          },
+          {
+            symptoms: 'Itchy blisters',
+            cause: 'shingles, chickenpox'
+          }
+        ],
+        headers: [ 'Symptoms', 'Possible cause' ]
+      }
+    }
+  }
+</script>
